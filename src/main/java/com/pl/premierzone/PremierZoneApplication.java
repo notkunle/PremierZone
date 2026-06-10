5585e171abeb70de06bcf613d8ba967966158b85
notkunle <kfagebo@gmail.com>
2026-06-10 17:12:40 -0400

    Initial commit

--- src/main/java/com/pl/premierzone/PremierZoneApplication.java
+++ src/main/java/com/pl/premierzone/PremierZoneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PremierZoneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PremierZoneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PremierZoneApplication.class, args);
+    }
 
 }
