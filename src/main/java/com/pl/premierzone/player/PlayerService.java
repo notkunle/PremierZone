eee0543e262e3202f4415c9ca5c22ff9f1bd3eaa
notkunle <kfagebo@gmail.com>
2026-06-16 19:02:46 -0400

    added more stuff to the repository

--- src/main/java/com/pl/premierzone/player/PlayerService.java
+++ src/main/java/com/pl/premierzone/player/PlayerService.java
@@ -1,4 +1,38 @@
 package com.pl.premierzone.player;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+
 public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> getPlayers() {
+        return playerRepository.findAll();
+    }
+
+    public List<Player> getPlayersFromTeam(String teamName) {
+        return playerRepository.findAll().stream().filter(player -> teamName.equals(player.getTeam())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByName(String searchText) {
+        return playerRepository.findAll().stream().filter(player -> player.getName().toLowerCase().contains(searchText.toLowerCase())).collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPosition(String searchText) {
+        return playerRepository.findAll().stream().filter(player -> player.getPos().toLowerCase().contains(searchText.toLowerCase())).collect(Collectors.toList());
+    }
+
+
+
 }
